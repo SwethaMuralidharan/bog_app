4474a571dd70796798b171b33a2ce40507faa7b3
Swetha Muralidharan <swethamuralidharan@Swethas-MacBook-Pro.local>
2018-01-08 17:12:54 -0800

    pushed app_1

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -2,4 +2,34 @@ class CreaturesController < ApplicationController
   def index
     @creatures=Creature.all
   end
+
+  def new
+    @creature=Creature.new
+  end
+
+  def create
+    creature= Creature.new(creature_params)
+    if creature.save
+      redirect_to creatures_path(creature)
+    end
+  end
+
+  def show
+    @creature=Creature.find_by_id(params[:id])
+  end
+
+  def edit
+    @creature=Creature.find_by_id(params[:id])
+  end
+
+  def update
+    creature= Creature.update_attributes(creature_params)
+    redirect_to creatures_path(creature)
+  end
+
+  private
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root "creatures#index" # localhost:3000 = root
-  get "/creatures", to: "creatures#index" # localhost:3000/creatures
+  get "/creatures", to: "creatures#index", as: "creatures"# localhost:3000/creatures
+  get "/creatures/new", to: "creatures#new", as: "new_creature"
+  post "/creatures", to: "creatures#create", as: "saved_creature"
+  get "/creatures/:id", to: "creatures#show", as: "selected_creature"
+  get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
+  patch "/creatures/:id", to: "creatures#update"
 end
